c3d56847988a1883832d1cf34c2d43ac2a8eabc5
Sanish Thapa <thapasanish73@gmail.com>
2022-09-06 20:52:51 +0545

    asd

---
+++ app/init.php
@@ -0,0 +1,7 @@
+<?php
+
+// here init is created to include important files 
+require('../app/core/app.php')
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+//this is one page website where every page is included in this page
+
+
+require("../app/init.php");
+
+$app = new app();
+
+?>
