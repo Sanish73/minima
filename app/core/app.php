e035a92156d2c6e9d76fc6c21d3f6288c038dcb2
Sanish Thapa <thapasanish73@gmail.com>
2022-09-06 22:45:44 +0545

    daf

--- app/core/app.php
+++ app/core/app.php
@@ -1,27 +1,25 @@
 <?php
 
-class app{
+class app
+{
 
     private $controller  = "home"; ///this is from controllers/home.php
     private $method = "index";
     private $params = [];
-  
-   public  function __construct()
+
+    public  function __construct()
     {
         // echo("hello");
         // print_r($_GET); //this is print readable 
         $url = $this->splitURL();
-      
-    show($url);
-      
-    } 
-    
 
-    private function splitURL()//public beacause we want to use it outside the class
+        show($url);
+    }
+
+
+    private function splitURL() //public beacause we want to use it outside the class
     {
 
-        return explode('/', $_GET['url']);
-
+        return explode('/', trim($_GET['url'],'/'));
     }
-       
 }
