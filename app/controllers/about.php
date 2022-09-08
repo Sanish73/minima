94a01d28f28838a505376a870f18b583c328c7db
Sanish Thapa <thapasanish73@gmail.com>
2022-09-08 13:00:47 +0545

    dfgfdg

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -3,7 +3,10 @@
 class About extends Controller{
 //this is for not talking empty value from URL
     function index(){
+     
         $this->view("about");
+
+      
     }
 
 
--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,7 +2,12 @@
 
 class Home extends Controller{
 //this is for not talking empty value from URL
-    function index(){
+    function index($c=''){
+        // show($a);
+        // show($b);
+        show($c);
+        $image_class  = $this->loadModel("image_class");
+        // show($image_class);
         $this->view("home");
     }
 
