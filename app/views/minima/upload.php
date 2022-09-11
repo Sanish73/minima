c4f5c3fb71086ddf864dd259e579970a192de40a
Sanish Thapa <thapasanish73@gmail.com>
2022-09-12 00:15:40 +0545

    sadf

--- app/views/minima/upload.php
+++ app/views/minima/upload.php
@@ -1,26 +1,28 @@
 <?php $this->view("minima/header", $data); ?>
 
-<header class="section background-white">
-    <div style="min-height: 200px; background-color: none;">
+<section class="section background-white">
+    <div class="s-12 m-12 l-4 center">
+        <h4 class="text-size-20 margin-bottom-20 text-dark text-center">Upload Image</h4>
+        <form name="contactForm" class="customform" method="post">
+            
+            <div class="s-12">
+                <input name="subject" class="subject" placeholder="Subject" title="Subject" type="text">
+                <p class="subject-error form-error">Please enter the subject.</p>
+            </div>
 
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <div class="s-12">
+                <input name="file" class="subject" type="file">
+                <p class="subject-error form-error">Please Select a File</p>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+
+            <div class="s-12">
+                <textarea name="message" class="required message" placeholder="Description" rows="3"></textarea>
+                <p class="message-error form-error">Please enter your message.</p>
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="s-12"><button class="s-12 submit-form button background-primary text-white" type="submit">Submit Button</button></div>
         </form>
     </div>
-</header>
+</section>
 
 
 <?php $this->view("minima/footer", $data); ?>
