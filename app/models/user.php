da6e417e4a91d8409f538112e7e371b9eef80baf
Sanish Thapa <thapasanish73@gmail.com>
2022-09-12 14:55:21 +0545

    sdfa

--- app/models/user.php
+++ app/models/user.php
@@ -17,22 +17,55 @@ class User
 
             if (is_array($data)) {
                 //logged in
-                $_SESSION['user-id'] = $data[0]->userid;
-                $_SESSION['user-name'] = $data[0]->username;
-            }else{
-                $_SESSION['error'] = "Wrong  username and password"
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->url_address;
+            } else {
+                $_SESSION['error'] = "Wrong  username and password";
             }
-        }else{
-            $_SESSION['error'] = "Please Enter valid username and password"
+        } else {
+            $_SESSION['error'] = "Please Enter valid username and password";
         }
     }
 
     function signup()
     {
+        $DB = new Database();
+        $_SESSION['error'] = '';
+
+        if (isset($_POST['username']) && isset($_POST['password']) && isset( $_POST['email'])) {
+            $arr['username'] = $_POST['username'];
+            $arr['password'] = $_POST['password'];
+            $arr['email'] = $_POST['email'];
+            $qry = "INSERT INTO users (username , password, email) values(:username, :password, :email)";
+            $data = $DB->write($qry, $arr);
+
+            if ($data) {
+               header("Location:".ROOT."login");
+               die;
+            } 
+
+        } else {
+            $_SESSION['error'] = "Please Enter valid username and password";
+        }
     }
 
     function check_loged_in()
     { //check whether user is login or not
+        $DB = new Database();
+        if (isset($_SESSION['user_url'])) {
+            $arr['user_url'] = $_SESSION['user_url'];
+            $qry = "SELECT * FROM users WHERE user_address = :user_url  limit 1 ";
+            $data = $DB->read($qry, $arr);
 
+            if (is_array($data)) {
+                //logged in
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->url_address;
+                return true;
+            }
+        }
+        return false;
     }
 }
