537090c875f07419330d10340405bd142ca38bc4
Sanish Thapa <thapasanish73@gmail.com>
2022-09-12 14:32:12 +0545

    zczxc

--- app/views/minima/login.php
+++ app/views/minima/login.php
@@ -100,19 +100,20 @@ body {
   -moz-osx-font-smoothing: grayscale;      
 }
 </style>
+<?php $this->view("minima/header" , $data);?>
 
-<div class="login-page">
+<div class="login-page"><h2>Login</h2>
   <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
+    <form class="register-form" method="POST">
+      <input type="text" name="username" placeholder="name"/>
+      <input type="password" name="password" placeholder="password"/>
+      <input type="text" name="email" placeholder="email address"/>
       <button>create</button>
       <p class="message">Already registered? <a href="#">Sign In</a></p>
     </form>
     <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
+      <input type="text" name="username" placeholder="username"/>
+      <input type="password" name="password" placeholder="password"/>
       <button>login</button>
       <p class="message">Not registered? <a href="#">Create an account</a></p>
     </form>
@@ -125,3 +126,4 @@ $('.message a').click(function(){
 });
 </script>
 
+<?php $this->view("minima/footer" , $data);?>
