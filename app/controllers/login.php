b0a99bc329f2f27b8697c06066b31f68eb0d1d06
Sanish Thapa <thapasanish73@gmail.com>
2022-09-14 20:29:41 +0545

    asd

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,30 +1,55 @@
+
+
+// class Login extends Controller
+// {
+//     //this is for not talking empty value from URL
+//     function index()
+//     {
+//         // show($a);
+//         // show($b);
+//         // show($c);
+//         // $DB = new Database();
+//         // $data['code']  = $DB->read("SELECT * FROM images");
+       
+//         // show($data[0]->image);
+//         // show($DB);
+//         // $image_class  = $this->loadModel("image_class");
+//         // show($image_class);
+//         $data['WEBSITE_TITLE'] = "Login"; 
+//         // $data['page_title'] = "web page"; 
+//         echo $_POST['email'];
+//         // if(null!==($_POST('email'))){
+//         //     $user = $this->loadModel("user");
+//         //     $user->signup($_POST);
+//         // }elseif(null!==($_POST('username')) && !null!==($_POST('username'))){
+//         //     $user = $this->loadModel("user");
+//         //     $user->login($_POST);
+//         // }
+//         $this->view("minima/login" , $data);
+//     }
+// }
+
 <?php
 
-class Login extends Controller
+Class Login extends Controller 
 {
-    //this is for not talking empty value from URL
-    function index()
-    {
-        // show($a);
-        // show($b);
-        // show($c);
-        // $DB = new Database();
-        // $data['code']  = $DB->read("SELECT * FROM images");
-       
-        // show($data[0]->image);
-        // show($DB);
-        // $image_class  = $this->loadModel("image_class");
-        // show($image_class);
+	function index()
+	{
+ 	 	
         $data['WEBSITE_TITLE'] = "Login"; 
-        // $data['page_title'] = "web page"; 
-        echo $_POST['email'];
-        // if(null!==($_POST('email'))){
-        //     $user = $this->loadModel("user");
-        //     $user->signup($_POST);
-        // }elseif(null!==($_POST('username')) && !null!==($_POST('username'))){
-        //     $user = $this->loadModel("user");
-        //     $user->login($_POST);
-        // }
-        $this->view("minima/login" , $data);
-    }
+
+ 	 	if(isset($_POST['email']))
+ 	 	{
+ 	 		$user = $this->loadModel("user");
+ 	 		$user->signup($_POST);
+
+ 	 	}elseif(isset($_POST['username']) && !isset($_POST['email'])){
+
+ 	 		$user = $this->loadModel("user");
+ 	 		$user->login($_POST);
+ 	 	}
+
+		$this->view("minima/login",$data);
+	}
+
 }
--- app/init.php
+++ app/init.php
@@ -1,10 +1,11 @@
 <?php
 
 // here init is created to include important files 
-require('../app/core/config.php');
-require('../app/core/database.php');
-require('../app/core/controller.php');
-require('../app/core/app.php');
-require('../app/core/functions.php');
+
+require "../app/core/config.php";
+require "../app/core/functions.php";
+require "../app/core/database.php";
+require "../app/core/controller.php";
+require "../app/core/app.php";
 
 ?>
