2d6069c004d618fa977f9d6298e8e1fb8f05fcd7
Sanish Thapa <thapasanish73@gmail.com>
2022-09-16 15:23:23 +0545

    sdf

--- app/models/upload_file.php
+++ app/models/upload_file.php
@@ -3,5 +3,23 @@
 
 class Upload_file
 {
+    function upload()
+    {
+        $DB = new Database();
+        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+            $arr['username'] = $_POST['username'];
+            $arr['password'] = $_POST['password'];
+            $arr['email'] = $_POST['email'];
+            $arr['url_address'] =  get_random_string_max(60);
+            $arr['date'] = date("y-m-d H:i:s");
 
+            $qry = "INSERT INTO users (username , password, email, url_address , date) values(:username, :password, :email, :url_address , :date)";
+            $data = $DB->write($qry, $arr);
+
+            if ($data) {
+                header("Location:" . ROOT . "login");
+                die;
+            }
+        }
+    }
 }
