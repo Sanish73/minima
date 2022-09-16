b2eb4b6bac4e522b5719c723eac695ccb3dcbea4
Sanish Thapa <thapasanish73@gmail.com>
2022-09-16 16:46:52 +0545

    asd

--- app/models/upload_file.php
+++ app/models/upload_file.php
@@ -3,21 +3,22 @@
 
 class Upload_file
 {
-    function upload()
+    function upload($POST , $FILES)
     {
         $DB = new Database();
-        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
-            $arr['username'] = $_POST['username'];
-            $arr['password'] = $_POST['password'];
-            $arr['email'] = $_POST['email'];
+        if (isset($_POST['title']) && isset($_POST['description']) && isset($_FILES['file'])) {
+            //uploading file
+            $arr['title'] = $_POST['title'];
+            $arr['description'] = $_POST['description'];
+            
             $arr['url_address'] =  get_random_string_max(60);
             $arr['date'] = date("y-m-d H:i:s");
 
-            $qry = "INSERT INTO users (username , password, email, url_address , date) values(:username, :password, :email, :url_address , :date)";
+            $qry = "INSERT INTO images (title , description, url_address , date) values(:title, :description, :url_address , :date)";
             $data = $DB->write($qry, $arr);
 
             if ($data) {
-                header("Location:" . ROOT . "login");
+                header("Location:" . ROOT . "home");
                 die;
             }
         }
