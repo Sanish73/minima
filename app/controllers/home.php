8e45edc8c176132dd78e77e9c1a41734a4893a23
Sanish Thapa <thapasanish73@gmail.com>
2022-09-17 23:14:45 +0545

    sdf

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -21,21 +21,23 @@ class Home extends Controller
         $posts = $this->loadModel("posts");
         $result = $posts->get_all();
 
+        // show ($result);
+
         // $pagination = $this->loadModel("pagination");
         // $data['prev_page'] = $pagination->generate_link($pagination->current_page_number() - 1);
         // $data['next_page'] = $pagination->generate_link($pagination->current_page_number() + 1);
 
 
         $data['posts'] = $result;
-        // $image_class = $this->loadModel("image_class");
+        $image_class = $this->loadModel("image_class");
 
-        // if(is_array($data['posts']))
-        // {
-        //     foreach ($data['posts'] as $key => $value) {
-        //         # code...
-        //         $data['posts'][$key]->image = $image_class->get_thumbnail($data['posts'][$key]->image);
-        //     }
-        // }
+        if(is_array($data['posts']))
+        {
+            foreach ($data['posts'] as $key => $value) {
+                # code...
+                $data['posts'][$key]->images = $image_class->get_thumbnail($data['posts'][$key]->images);
+            }
+        }
       $this->view("minima/index",$data);
     }
 }
